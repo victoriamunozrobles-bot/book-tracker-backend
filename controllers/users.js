5e03c5190141f7067c8b279d86208a66a7affa6b
Victoria Muñoz <victoriamunozrobles@gmail.com>
2026-06-23 16:50:16 -0400

    Feat: implementa la creación de usuario y funcionalidad del login.

--- controllers/users.js
+++ controllers/users.js
@@ -14,3 +14,68 @@ module.exports.getCurrentUser = (req, res) => {
         .send({ message: "Error interno del servidor", error: err.message })
     );
 };
+
+module.exports.createUser = (req, res) => {
+  const { name, email, password } = req.body;
+
+  bcrypt
+    .hash(password, 10)
+    .then((hash) =>
+      User.create({
+        name,
+        email,
+        password: hash
+      })
+    )
+    .then((user) => {
+      const userResponse = user.toObject();
+      delete userResponse.password;
+      res.status(201).send(userResponse);
+    })
+    .catch((err) => {
+      if (err.name === "MongoServerError" && err.code === 11000) {
+        res
+          .status(409)
+          .send({ message: "El correo electrónico ya está registrado" });
+      } else {
+        res
+          .status(400)
+          .send({ message: "Datos inválidos", error: err.message });
+      }
+    });
+};
+
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res
+      .status(400)
+      .send({ message: "Se requiere correo electrónico y contraseña" });
+  }
+
+  User.findOne({ email })
+    .select("+password")
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error("Credenciales incorrectas"));
+      }
+
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error("Credenciales incorrectas"));
+        }
+        return user;
+      });
+    })
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, "super-secreto-dev", {
+        expiresIn: "7d"
+      });
+
+      res.send({ token });
+    })
+    .catch((err) => {
+      res.status(401).send({ message: err.message });
+    });
+};
