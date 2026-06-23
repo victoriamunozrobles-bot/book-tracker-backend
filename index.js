725d0dd594803d5994b0ea3562993ae88a91a435
Victoria Muñoz <victoriamunozrobles@gmail.com>
2026-06-23 16:56:44 -0400

    Feat: aggrega middleware de logging con Winston y actualiza dependencias.

--- index.js
+++ index.js
@@ -2,24 +2,24 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const mainRouter = require("./routes/index");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const app = express();
 const { PORT = 3000 } = process.env;
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/book_tracker_db")
-  .then(() => {
-    console.log("✅ Conectado exitosamente a MongoDB");
-  })
-  .catch((err) => {
-    console.error("❌ Error al conectar a MongoDB:", err);
-  });
+  .then(() => console.log("✅ Conectado exitosamente a MongoDB"))
+  .catch((err) => console.error("❌ Error al conectar a MongoDB:", err));
 
 app.use(cors());
-
 app.use(express.json());
 
+app.use(requestLogger);
+
 app.use("/", mainRouter);
+
+app.use(errorLogger);
 
 app.listen(PORT, () => {
   console.log(`🚀 Servidor backend arrancando en el puerto ${PORT}`);
