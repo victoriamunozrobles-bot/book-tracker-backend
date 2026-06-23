8d4d3bd70538b6010df101e8b58e4609371f4943
Victoria Muñoz <victoriamunozrobles@gmail.com>
2026-06-23 16:52:36 -0400

    Chore: actualiza dependencias y agrega nodemon para desarrollo.

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "El correo electrónico es obligatorio"],
+    unique: true,
+    validate: {
+      validator: function (v) {
+        return /^\S+@\S+\.\S+$/.test(v);
+      },
+      message: "Debes introducir un correo electrónico válido",
+    },
+  },
+  password: {
+    type: String,
+    required: [true, "La contraseña es obligatoria"],
+    select: false,
+  },
+  name: {
+    type: String,
+    required: [true, "El nombre es obligatorio"],
+    minlength: [2, "El nombre debe tener al menos 2 caracteres"],
+    maxlength: [30, "El nombre no puede exceder los 30 caracteres"],
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
