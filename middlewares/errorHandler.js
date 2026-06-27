9044ece94d40061e3b660e943d03780b86dde6c0
Victoria Muñoz <victoriamunozrobles@gmail.com>
2026-06-26 23:00:13 -0400

    Refactor: enhance error handling for MongoDB and validation errors in errorHandler middleware

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,16 @@
 module.exports = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
 
+  if (err.name === "MongoServerError" && err.code === 11000) {
+    return res
+      .status(409)
+      .send({ message: "El correo electrónico ya está registrado" });
+  }
+
+  if (err.name === "ValidationError" || err.name === "CastError") {
+    return res.status(400).send({ message: "Datos inválidos proporcionados" });
+  }
+
   res.status(statusCode).send({
     message:
       statusCode === 500 ? "Se ha producido un error en el servidor" : message
