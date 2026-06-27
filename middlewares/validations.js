62217479300f21dbe3ced20f8eab38c77ffffe3b
Victoria Muñoz <victoriamunozrobles@gmail.com>
2026-06-26 22:06:59 -0400

    Feat: add validation middleware for user signup and signin

---
+++ middlewares/validations.js
@@ -0,0 +1,18 @@
+const { celebrate, Joi } = require("celebrate");
+
+const validateSignup = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8)
+  })
+});
+
+const validateSignin = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8)
+  })
+});
+
+module.exports = { validateSignup, validateSignin };
